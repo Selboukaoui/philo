b12da37b3fbf3fc421b2f4de64acd40c785b9b22
Samir Elboukaoui <156188568+Selboukaoui@users.noreply.github.com>
2025-03-21 03:55:02 +0000

    Delete philo directory

--- philo/philo.c
+++
@@ -1,9 +0,0 @@
-#include "philo.h"
-
-int main(int ac, char **av)
-{
-    pthread_t   vars;
-    if (ac != 5 || ac != 6)
-        return (printf("Number of arguments is incorrect\n"), 0);
-    parsing(av);
-}
